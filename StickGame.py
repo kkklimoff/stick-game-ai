a85cb0b357ea7710576d0402f456b532c105ae76
Klims <klimatechnik1337@gmail.com>
2021-04-25 01:27:21 +0300

    Project started
    Added tree creation algorithm

---
+++ StickGame.py
@@ -0,0 +1,33 @@
+class Node(object):
+    def __init__(self, sticks, level):
+        self.sticks = sticks
+        self.level = level
+        self.children = []
+        self.addchildren()
+
+    def addchildren(self):
+        if self.sticks == 0:
+            return
+        elif self.sticks == 1:
+            self.children.append(Node(self.sticks - 1, self.switchlevel()))
+        else:
+            self.children.append(Node(self.sticks - 1, self.switchlevel()))
+            self.children.append(Node(self.sticks - 2, self.switchlevel()))
+
+    def switchlevel(self):
+        if self.level == 'MAX':
+            return 'MIN'
+        else:
+            return 'MAX'
+
+    def __str__(self, nlevel=0):
+        ret = '\t'*nlevel+repr(self.sticks)+'\n'
+        for child in self.children:
+            ret += child.__str__(nlevel+1)
+        return ret
+
+def main():
+    n = Node(7, 'MAX')
+    print(n)
+if __name__ == '__main__':
+    main()
